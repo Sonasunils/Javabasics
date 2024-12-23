472881eba5844c709f29e641b53b9eb52c5be384
Sonasunils <148698303+Sonasunils@users.noreply.github.com>
2024-12-23 21:51:41 +0530

    3) Swap two numbers

---
+++ Swap.java
@@ -0,0 +1,19 @@
+package workout;
+
+public class Swap {
+	public static void main(String args[]){
+		
+		    int m = 9, n = 5;
+		    int temp = m;
+	        m = n;
+	        n = temp;
+	        System.out.println("Value of m is " + m
+	                           + " and Value of n is " + n);
+		
+		
+		
+	
+		
+	}
+
+}
